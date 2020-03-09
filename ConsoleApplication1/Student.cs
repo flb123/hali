248d0616c0f83f76c6272838264bfe6607429639
flb123 <1981928832@qq.com>
2020-03-10 00:05:10 +0800

    第二个再次

---
+++ ConsoleApplication1/Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Student
+    {
+        string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        string age;
+
+        public string Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+        public Student()
+        {
+                
+        }
+    }
+}
